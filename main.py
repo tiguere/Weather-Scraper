22adc266ead4973849acbf8d76b4f204f3ae8cc8
yonatan <mirsky.yonatan9@gmail.com>
2021-02-25 14:05:44 +0200

    add get_next_day function add download_primary_data add download_secondary_data

--- main.py
+++ main.py
@@ -35,6 +35,7 @@ def get_next_day():
     soup = BeautifulSoup(day.content, 'html.parser')
     return soup
 
+
 def download_primary_data(soup):
     # create loop that looks for each element that contain the "hourly" forecast in the soup
     # account for the fact that there is overlap between days in the carousel
@@ -42,8 +43,17 @@ def download_primary_data(soup):
         # list_of_data = []
         # looking through "primary" elements
         # append to list_of_data
-    # return list_of_primary_data
-    pass
+    # return nested list_of_primary_data by each hour at day
+    data = []
+    hours_at_day = soup.find_all(class_="wr-time-slot-primary wr-js-time-slot-primary")
+    for hour in hours_at_day:
+        Time = hour.find('span', class_="wr-time-slot-primary__hours wr-u-font-weight-500").text
+        Temperature = hour.find('span', class_="wr-value--temperature--c").text
+        chance_of_precipitation = re.sub(r"[a-z]", '', hour.find('div', class_="wr-u-font-weight-500").text)
+        windspeed = hour.find('span', class_="wr-value--windspeed wr-value--windspeed--mph").text
+        data.append([Time, Temperature, chance_of_precipitation, windspeed])
+    return data
+
 
 def download_secondary_data(soup):
     # create loop that looks for each element that contain the "hourly" forecast in the soup
@@ -53,8 +63,17 @@ def download_secondary_data(soup):
         # looking through "secondary" elements
         # append to list_of_data
     # call write_to_file(list)
-    # return list_of_secondary_data
-    pass
+    # return nested list_of_secondary_data by each hour at day
+    data = []
+    secondary_list = soup.find_all("dl", class_="wr-time-slot-secondary__list")
+    for hour in secondary_list:
+        data_by_hour = hour.find_all('dd')
+        Humidity = data_by_hour[0].text
+        Pressure = data_by_hour[1].text
+        Temperature_C, Temperature_F = re.split(r'\D+', secondary_list[0].parent.next_sibling.text.split()[3])[:2]
+        data.append([Humidity, Pressure, Temperature_C, Temperature_F])
+    return data
+
 
 def write_to_file():
     pass
