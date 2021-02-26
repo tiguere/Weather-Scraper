e4066926ebf6942ce2fec7e75cd9a8bdeb5691f5
Namer <namermeduro@gmail.com>
2021-02-26 09:24:50 +0200

    edits to main

--- config.py
+++ config.py
@@ -1,2 +1,4 @@
-URL = "www.bbc.com/weather"
+URL = "https://www.bbc.com/weather/"
 COUNTRY_FILE_PATH = "PATH"
+FILENAME = "city_list.xlsx"
+FOLDER_NAME = "Cities"
--- main.py
+++ main.py
@@ -5,6 +5,7 @@ options = Options()
 options.add_argument('-headless')
 import pandas as pd
 from bs4 import BeautifulSoup
+import logging
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
@@ -17,10 +18,14 @@ import re
 import time
 import datetime
 import os
+import config as CFG
 
-URL = "https://www.bbc.com/weather/"
-FILENAME = "city_list.xlsx"
-FOLDER_NAME = "Cities"
+
+## CONFIGURATION OF LOGGER
+logging.basicConfig(
+    filename='movies.log',
+    format='%(asctime)s-%(levelname)s-FILE:%(filename)s-FUNCTION:%(funcName)s-LINE:%(lineno)d-%(message)s',
+    level=logging.INFO)
 
 
 def get_city_page(city, url):
@@ -30,8 +35,15 @@ def get_city_page(city, url):
     search = browser.find_element_by_xpath('//*[@id="ls-c-search__input-label"]')
     search.send_keys(city)
     time.sleep(2)
-    first_result = browser.find_element_by_xpath('//*[@id="location-list"]/li[2]/a')
-    first_result.click()
+    try:
+        first_result = browser.find_element_by_xpath('//*[@id="location-list"]/li[2]/a')
+        first_result.click()
+    except RuntimeError:
+        logging.info(f'UNSUCCESSFUL CLICK: MOVING ON..')
+        city_page = False
+        pass
+    else:
+        logging.info(f'SUCCESSFULY FOUND {city} IN DROPDOWN')
     city_page = browser.current_url
     return city_page
 
@@ -42,20 +54,21 @@ def get_next_day(link):
     return soup
 
 
-def download_primary_data(day_num, soup):
+def download_primary_data(city, day_num, soup):
     today = datetime.date.today()
+    hours_at_day = soup.find_all(class_="wr-time-slot-primary wr-js-time-slot-primary")
     date = today + datetime.timedelta(days=day_num)
-    date_string = date.strftime('%Y-%m-%d')
-    hours = soup.findAll('span',{'class':'wr-time-slot-primary__hours'})
-    temperatures = soup.findAll('span', {'class': 'wr-value--temperature--c'})
-    chance_of_precipitations = soup.findAll('div', {'class': 'wr-time-slot-primary__precipitation'})
-    wind_speeds = soup.findAll('span', {'class': 'wr-value--windspeed'})
-    print(wind_speeds)
-    for (hour, temp, chance, wind) in zip(hours, temperatures, chance_of_precipitations, wind_speeds):
-        chance_value = chance.find('div', {'class': "wr-u-font-weight-500"})
-        hour_record = [date_string]
-        hour_record.extend([hour.text, temp.text, chance_value.text, wind.text])
-    return hour_record
+    data = []
+    for hour in hours_at_day:
+        time_of_day = hour.find('span', class_="wr-time-slot-primary__hours wr-u-font-weight-500").text
+        if 0 <= int(time_of_day) <= 5:
+            date = today + datetime.timedelta(days=day_num+1)
+        date_string = date.strftime('%Y-%m-%d')
+        temperature = hour.find('span', class_="wr-value--temperature--c").text
+        chance_of_precipitation = re.sub(r"[a-z]", '', hour.find('div', class_="wr-u-font-weight-500").text)
+        wind_speed = hour.find('span', class_="wr-value--windspeed wr-value--windspeed--mph").text
+        data.append([city, date_string, time_of_day, temperature, chance_of_precipitation, wind_speed])
+    return data
 
 
 def download_secondary_data(soup):
@@ -67,7 +80,16 @@ def download_secondary_data(soup):
         # append to list_of_data
     # call write_to_file(list)
     # return list_of_secondary_data
-    pass
+    data = []
+    secondary_list = soup.find_all("dl", class_="wr-time-slot-secondary__list")
+    for hour in secondary_list:
+        data_by_hour = hour.find_all('dd')
+        humidity = data_by_hour[0].text
+        pressure = data_by_hour[1].text
+        temperature_c, temperature_f = re.split(r'\D+', secondary_list[0].parent.next_sibling.text.split()[3])[:2]
+        data.append([humidity, pressure, temperature_c])
+    return data
+
 
 def write_to_file():
     pass
@@ -75,17 +97,25 @@ def write_to_file():
 
 def main():
     current_path = os.path.abspath(os.getcwd())
-    file_path = os.path.join(current_path, FOLDER_NAME, FILENAME)
+    file_path = os.path.join(current_path, CFG.FOLDER_NAME, CFG.FILENAME)
     df = pd.read_excel(file_path)
     df["location"] = df["city"] + ", " + df["country"]
     cities = list(df["location"])
     for city in cities:
-        link = get_city_page(city, URL)
-        print(link)
-        for day in range(0, 14):
-            day_soup = get_next_day(link+"/day"+str(day))
-            download_primary_data(day, day_soup) #primary =
-        break
+        link = get_city_page(city, CFG.URL)
+        if link:
+            for day in range(0, 14):
+                day_soup = get_next_day(link+"/day"+str(day))
+                primary = download_primary_data(city, day, day_soup)
+                secondary = download_secondary_data(day_soup)
+                zipped = list(zip(primary, secondary))
+                df = pd.DataFrame(zipped)
+                if day == 0:
+                    df.to_csv('test.csv', mode='a', header=True)
+                else:
+                    df.to_csv('test.csv', mode='a', header=False)
+            break
+
         # download_secondary_data
         # write_to_file
     pass
